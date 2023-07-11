3a05b46d4a4379fddbde1a889a8a5b5ef19b9ad4
ict318 <polychubu.ict@gmail.com>
2023-07-11 15:40:59 +0900

    Initial commit

---
+++ app/src/main/java/com/websarva/wings/android/lifecyclesample/MainActivity.java
@@ -0,0 +1,14 @@
+package com.websarva.wings.android.lifecyclesample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
